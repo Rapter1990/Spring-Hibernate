a577fba11f746e84bbab275a9b2e7c34d1fb5ae1
Rapter <sngermiyanoglu@hotmail.com>
2018-11-09 15:04:30 +0300

    Add files via upload

---
+++ 003_HibernateMavenXML/src/org/germiyanoglu/modal/Management.java
@@ -0,0 +1,49 @@
+package org.germiyanoglu.modal;
+
+import java.util.logging.Logger;
+
+import org.hibernate.Session;
+
+import util.HibernateUtil;
+
+public class Management {
+	
+	private static Session session = null;
+	public final static Logger logger = Logger.getLogger(Management.class.getName());
+
+	
+	public static void main(String[] args) {
+		
+		Personel personel = new Personel();
+		personel.setAd("Aslý");
+		personel.setSoyad("Þafak");
+		personel.setEmail("b@b.com");
+		
+		try {
+			session = HibernateUtil.openSessionFactory();
+			session.beginTransaction();
+			
+			session.save(personel);
+			
+			session.getTransaction().commit();
+			logger.info("\n.................Database eklendi!..................\n");
+
+		}catch(Exception sqlException) {
+			if(session.getTransaction() != null ) { // .isActive() 
+				logger.info("\n.......Transaction Is Being Rolled Back.......\n");
+				session.getTransaction().rollback();
+				sqlException.printStackTrace();
+			}			        
+		}finally {
+			// TODO: handle finally clause
+			if(session != null) {
+				logger.info("\n...............Session Kapandý...............\n");
+				session.close();
+			}
+		}
+		
+		
+		
+	}
+
+}
